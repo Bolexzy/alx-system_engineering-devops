9b1fdb0b1373b4af060a3baba7aa1f3fa5d7955b
Bolexzy <boluoyewumi79@gmail.com>
2023-04-04 12:30:07 +0100

    [Update file]: Fix http header block

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,10 +5,12 @@ package { 'nginx':
     provider => 'apt',
 }
 
-file_line { 'http header':
-    path  => '/etc/nginx/nginx.conf',
-    match => 'server_name _;',
-    line  => 'server_name _;\n\n\tadd_header X-Served-By \$HOSTNAME;',
+exec {'100 allow nginx':
+    command => '/usr/sbin/ufw allow "Nginx HTTP"'
+}
+
+exec { 'http header':
+    command => '/usr/bin/sed -i "s/http {/http {\n\tadd_header X-Served-By $(hostname);/" /etc/nginx/nginx.conf',
 }
 
 exec { 'run':
