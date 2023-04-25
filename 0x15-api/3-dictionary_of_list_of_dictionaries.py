ed6d40700c23f1322862c6a1b3fd5f54eea3912e
Bolexzy <boluoyewumi79@gmail.com>
2023-04-25 15:25:30 +0100

    Add file: export data in json format of all employees

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Export to-do information of all emoployee to json format
+using JSONPlaceholder API
+(https://jsonplaceholder.typicode.com/)
+"""
+
+import json
+import requests
+from sys import argv
+
+url = 'https://jsonplaceholder.typicode.com/'
+
+
+if __name__ == "__main__":
+    users = requests.get(url + 'users').json()
+    todos = requests.get(url + 'todos').json()
+
+    filename = 'todo_all_employees.json'
+
+    with open(filename, 'w') as jsonfile:
+        json.dump({
+            u.get('id'): [{
+                "username": u.get('username'),
+                "task": t.get('title'),
+                "completed": t.get('completed'),
+                } for t in todos]
+            for u in users}, jsonfile)
